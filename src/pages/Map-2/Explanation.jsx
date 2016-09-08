dd3a6b98e18473c4ed34897d9a773f4b31e81fc4
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-09-08 15:58:36 +0100

    Add Map-2 to index

---
+++ src/pages/Map-2/Explanation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Explanation extends React.Component {
+  render() {
+    return (
+      <div className="map-explanation">
+        <h1>Adding Multiple Tile and Data Layers</h1>
+        <p>...</p>
+        <p>Accurate as of 2016.09.07.</p>
+      </div>
+    );
+  }
+}
