b94f36f499da50592962e5d4a205eaa115c78725
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2016-09-22 17:49:06 +0100

    Added Map-7: Adding a Recentre Map Button

--- src/components/App.js
+++ src/components/App.js
@@ -30,6 +30,7 @@ export default class App extends React.Component {
                 <li><Link to="/map-4" className="link">Map 4:</Link> Create Dynamic Layer Styles</li>
                 <li><Link to="/map-5" className="link">Map 5:</Link> Create Interactive Layers</li>
                 <li><Link to="/map-6" className="link">Map 6:</Link> Add Interactive, Dynamic Data Markers</li>
+                <li><Link to="/map-7" className="link">Map 7:</Link> Add a "Recentre Map" Button</li>
               </ul>
               <p>Each Map is built upon the previous Map, adding new features along the way.</p>
               <h2>Source Code</h2>
---
+++ src/pages/Map-7/Explanation.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default class Explanation extends React.Component {
+  render() {
+    return (
+      <div className="map-explanation">
+        <h1>Adding a "Recentre Map" Button</h1>
+        <p>Let's add a simple button that recentres the map view. Partly to make navigating the map convenient for users, but mostly to show off custom controls and programmatic actions within the Leaflet Map.</p>
+        <h2>Adding the 'Control'</h2>
+        <p>What we're adding is adding a generic control, so we use <b>L.control()</b></p>
+        <code dangerouslySetInnerHTML={{__html:
+          'const recentreButton = L.control({position: \'topleft\'});'
+        }}/>
+        <p>The trick is how we define the control. This is done on the the control's <b>.onAdd()</b> function, which we need to define to return our "Recentre Map" button.</p>
+        <code dangerouslySetInnerHTML={{__html:
+          'recentreButton.onAdd = (map) => { \n' +
+          '  const button = L.DomUtil.create(\'button\', \'btn fa fa-crosshairs\'); \n' +
+          '  button.onclick = () => { \n' +
+          '    myMap.setZoom(10); \n' +
+          '    myMap.panTo([-18.8, 34.5]); \n' +
+          '  }; \n' +
+          '  return button; \n' +
+          '};'
+        }}/>
+        <p>The most important thing to note here is that we're defining our "Recentre Map" button (i.e. the actual HTML DOM object) using <b>L.DomUtil</b> instead of using a React Component.</p>
+        <p>This is a knowledge limitation since - although we know how to add Leaflet into React - adding React into Leaflet's a bit a mystery at the moment.</p>
+        <p>On a minor note, we used <a href="http://fontawesome.io/">Font Awesome</a> to give the button a nice <span className="fa fa-crosshairs"></span> crosshairs icon.</p>
+        <p>As usual, once we're done, we add it to the map and call it a day.</p>
+        <code dangerouslySetInnerHTML={{__html:
+          'recentreButton.addTo(myMap);'
+        }}/>
+        <p>Accurate as of 2016.09.22.</p>
+      </div>
+    );
+  }
+}
